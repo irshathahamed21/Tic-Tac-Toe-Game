6c90e14fbe810b9975c1c32cfff6bafd31bf0343
irshathahamed21 <21irshathkhan@gmail.com>
2022-01-12 17:15:59 +0530

    Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,15 +3,51 @@ import { Square } from './Square';
 import {useState} from "react"
 function Board(){
 const[squares, setSquares] = useState(Array(9).fill(null))
+const[xIsNext, setXIsNext] = useState(true)
 
-    function renderSquare(i) {
-        return <Square value = {i}/>
+
+    console.log(squares)
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
     }
 
-    const status = "nextplayer: X"
+    else {
+        
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    function handleClick(i){
+
+         if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+        const newSquares = squares.slice()
+        newSquares[i] = xIsNext ? "X": "O"
+        setSquares(newSquares)
+        setXIsNext(!xIsNext)
+
+    }
+    function renderSquare(i) {
+        return (<Square 
+                    value = {squares[i]}
+                    onClick = { ()=> handleClick(i)}/>
+        )
+    }
+
+
+
+
+
+
+
+
+   
     return (
         <>
      <div>
+     < h1 className = "titl">Tic Tac Toe</h1>
       <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
@@ -35,5 +71,28 @@ const[squares, setSquares] = useState(Array(9).fill(null))
         </>
     )
 
-}
-export {Board}
+
+    }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+ 
+
+  export {Board}
