09a9235a082a20b0f38ce4b887d769749871ce02
irshathahamed21 <21irshathkhan@gmail.com>
2022-01-13 08:51:48 +0530

    updated files

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,9 @@
 import '../App.css';
-import {useState} from "react"
+
 
 function Square(props){
 
-const[value, setValue] = useState(null)
+
     return (
         <>
         <button className = "square" 
